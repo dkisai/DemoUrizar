d91ac3c08175f275a0534fddb0d66da1d0a708ed
dkisai <diego.kisai@live.com>
2022-03-18 16:58:04 -0600

    mostrar elementos con el scroll

---
+++ test.js
@@ -0,0 +1,11 @@
+let animado = document.querySelectorAll('.animado');
+function mostrarScroll(){
+    let scrollTop = document.documentElement.scrollTop;
+    for(var i=0; i<animado.length; i++){
+        let alturaAnimado = animado[i].offsetTop;
+        if(alturaAnimado- 500 < scrollTop){
+            animado[i].style.opacity = 1;
+        }
+    }
+}
+window.addEventListener('scroll',mostrarScroll);
