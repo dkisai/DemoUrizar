ab6b58bbc45bd386dc4e35460ee4306d0a2f6a66
dkisai <diego.kisai@live.com>
2022-03-18 17:06:15 -0600

    Cambios menores para el scroll

--- test.js
+++ test.js
@@ -3,8 +3,9 @@ function mostrarScroll(){
     let scrollTop = document.documentElement.scrollTop;
     for(var i=0; i<animado.length; i++){
         let alturaAnimado = animado[i].offsetTop;
-        if(alturaAnimado- 500 < scrollTop){
+        if(alturaAnimado - 500 < scrollTop){
             animado[i].style.opacity = 1;
+            animado[i].classList.add('mostrarArriba')
         }
     }
 }
